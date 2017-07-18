2d1ae0ef40405c996defc77001b7a92c88623060
Ohmygodvt95 <ohmygodvt95@gmail.com>
2017-07-18 17:17:14 +0700

    126960 seach book by title, ibns

--- Gemfile
+++ Gemfile
@@ -15,15 +15,13 @@ gem "turbolinks", "~> 5"
 gem "jbuilder", "~> 2.5"
 gem "config"
 gem "devise"
+gem "ransack"
 
 group :development, :test do
   gem "byebug", platforms: [:mri, :mingw, :x64_mingw]
   gem "capybara", "~> 2.13"
   gem "selenium-webdriver"
   gem "rspec-rails"
-  gem "database_cleaner"
-  gem "factory_girl_rails"
-  gem "faker"
   gem "rails-controller-testing"
 end
 
@@ -32,6 +30,8 @@ group :development do
   gem "listen", ">= 3.0.5", "< 3.2"
   gem "spring"
   gem "spring-watcher-listen", "~> 2.0.0"
+  gem "better_errors"
+  gem "binding_of_caller"
 end
 
 gem "tzinfo-data", platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- app/assets/javascripts/users/layouts/header.js
+++ app/assets/javascripts/users/layouts/header.js
@@ -13,3 +13,32 @@ $( '.search-toggle' ).click(function() {
     $('.navbar-nav').addClass('hide');
   }
 });
+
+var timer;
+
+$('#search').keyup(function(){
+  $('#autocomplete').html($('#searching').html());
+
+  clearTimeout(timer);
+
+  var ms = 500;
+  var val = this.value;
+
+  timer = setTimeout(function() {
+    search(val);
+  }, ms);
+});
+
+function search(query) {
+  if(query.length > 0){
+    var url = '/search.json?query=' + query;
+    $.get(url, function (data, status) {
+      if(status === 'success'){
+        $('#autocomplete').html($(data.html_data));
+      }
+    });
+  }
+  else {
+    $('#autocomplete').html('');
+  }
+}
