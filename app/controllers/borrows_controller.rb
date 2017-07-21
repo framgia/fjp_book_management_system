6f75b861bd00b05d589523d29f723a64673f2064
Strawberry <strawberrycsgo@gmail.com>
2017-07-21 09:20:48 +0700

    126966 book borrow

---
+++ app/controllers/borrows_controller.rb
@@ -0,0 +1,23 @@
+class BorrowsController < ApplicationController
+  include ApplicationHelper
+
+  def create
+    borrow = Borrow.new
+
+    unless borrow.update_attributes borrow_params.merge user_id: current_user.id
+      flash[:danger] = t "borrows.create.flash.fail_create"
+    end
+
+    respond_to do |f|
+      partial = "books/_modal_send_req_result"
+      book_id = borrow_params[:book_id]
+      f.html{render partial, layout: false, locals: {book: find_book(book_id)}}
+    end
+  end
+
+  private
+  def borrow_params
+    params.require(:borrow).permit :book_id, :user_id, :note,
+      :time_start, :time_end
+  end
+end
