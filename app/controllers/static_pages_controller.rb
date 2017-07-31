571e6e0626370421b530fd4344e56f481b459972
Strawberry <strawberrycsgo@gmail.com>
2017-07-31 08:17:53 +0700

    127816 homepage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,17 @@
 class StaticPagesController < ApplicationController
+  include ApplicationHelper
+
+  def index; end
+
   def show
+    setting = Settings.user.home
+    @support = Supports::Home.new new_book:
+      Book.new_book(setting.new_book),
+      popular_book: Book.popular_book(setting.popular_book),
+      recent_blog: Blog.recent_blog(setting.recent_blog),
+      top_author: Author.top_author,
+      top_rate_book: Book.top_rated_book(setting.top_rate_book)
+
     if valid_page?
       render "static_pages/#{params[:page]}"
     else
