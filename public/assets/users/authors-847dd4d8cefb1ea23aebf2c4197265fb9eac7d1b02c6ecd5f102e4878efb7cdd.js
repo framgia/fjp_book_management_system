4174e422565a1859510954781486aef22080c16d
Strawberry <strawberrycsgo@gmail.com>
2017-08-08 09:44:39 +0700

    assets

---
+++ public/assets/users/authors-847dd4d8cefb1ea23aebf2c4197265fb9eac7d1b02c6ecd5f102e4878efb7cdd.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$("#author-book-slider").owlCarousel({margin:0,loop:!1,lazyLoad:!0,items:3,responsive:{0:{items:2},768:{items:3,margin:15},1024:{items:3},1440:{items:4}}}),$(".fa-angle-left").click(function(){$(this).parent().next().trigger("prev.owl.carousel")}),$(".fa-angle-right").click(function(){$(this).parent().next().trigger("next.owl.carousel")})});
